b527b439ae04c340be46fe452a8cef8e1aa36750
NishantIyer <nishant.iyer2609@outlook.com>
2023-06-03 11:46:28 +0000

    Commit 24

---
+++ 0d515b52a5a12ef2.asm
@@ -0,0 +1,39 @@
+; Block 1 - 0d515b52a5a12ef2.asm
+mov eax, 1
+mov ebx, 2
+add eax, ebx
+sub eax, 3
+cmp eax, 0
+jg label_1
+jmp exit
+label_1:
+mov ecx, 4
+mul ecx
+exit:
+
+; Block 2 - 0d515b52a5a12ef2.asm
+mov eax, 1
+mov ebx, 2
+add eax, ebx
+sub eax, 3
+cmp eax, 0
+jg label_2
+jmp exit
+label_2:
+mov ecx, 4
+mul ecx
+exit:
+
+; Block 3 - 0d515b52a5a12ef2.asm
+mov eax, 1
+mov ebx, 2
+add eax, ebx
+sub eax, 3
+cmp eax, 0
+jg label_3
+jmp exit
+label_3:
+mov ecx, 4
+mul ecx
+exit:
+
